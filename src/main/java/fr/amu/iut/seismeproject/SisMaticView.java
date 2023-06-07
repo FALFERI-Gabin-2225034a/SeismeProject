a71c0eb60269ec3bdf25a90cb26f3fdf2236843c
GabinFALFERIF22003745 <gabin.falferi@gmail.com>
2023-06-07 14:53:03 +0200

    Ajout du menu et de la fonctionnalité du premier bouton

--- src/main/java/fr/amu/iut/seismeproject/SisMaticView.java
+++ src/main/java/fr/amu/iut/seismeproject/SisMaticView.java
@@ -1,8 +1,60 @@
 package fr.amu.iut.seismeproject;
 
-public class SisMaticView {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class SisMaticView implements Initializable {
     private SisMaticViewModel viewModel;
+    @FXML
+    private VBox menu;
+    @FXML
+    private Button buttonMenu;
+    @FXML
+    private Button buttonFile;
+    @FXML
+    private Button buttonMap;
+    @FXML
+    private Button buttonDashboard;
+
+    public SisMaticView() {
+        viewModel = new SisMaticViewModel();
+    }
     public void setViewModel(SisMaticViewModel viewModel) {
         this.viewModel = viewModel;
+    }
+
+    @FXML
+    private void showMenu() {
+        ArrayList<Button> listButtons = new ArrayList<>();
+        listButtons.add(buttonMenu);
+        listButtons.add(buttonFile);
+        listButtons.add(buttonMap);
+        listButtons.add(buttonDashboard);
+        if (buttonMenu.getText() == "") {
+            for (Button button : listButtons)
+                viewModel.transformMenuButton(true, button);
+            menu.setAlignment(Pos.TOP_LEFT);
+            menu.setPrefWidth(350);
+        }
+        else {
+            for (Button button : listButtons)
+                viewModel.transformMenuButton(false, button);
+            menu.setAlignment(Pos.TOP_CENTER);
+            menu.setPrefWidth(100);
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
     }
 }
--- src/main/java/fr/amu/iut/seismeproject/SisMaticViewModel.java
+++ src/main/java/fr/amu/iut/seismeproject/SisMaticViewModel.java
@@ -1,9 +1,23 @@
 package fr.amu.iut.seismeproject;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
 public class SisMaticViewModel {
     private SisMaticModel model;
 
+    public SisMaticViewModel() {
+        model = new SisMaticModel();
+    }
     public void setModel(SisMaticModel model) {
         this.model = model;
+    }
+
+    public void transformMenuButton(boolean isVoid, Button button) {
+        if (isVoid)
+            button.setText("\t" + button.getId().substring(6));
+        else
+            button.setText("");
     }
 }
