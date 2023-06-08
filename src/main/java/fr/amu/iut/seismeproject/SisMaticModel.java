3d1ca508d84bff93346a3d0dd323314ec240c9c7
GabinFALFERIF22003745 <gabin.falferi@gmail.com>
2023-06-08 08:07:10 +0200

    Correction d'un bug

--- src/main/java/fr/amu/iut/seismeproject/SisMaticModel.java
+++ src/main/java/fr/amu/iut/seismeproject/SisMaticModel.java
@@ -3,7 +3,7 @@ package fr.amu.iut.seismeproject;
 import java.io.File;
 
 public class SisMaticModel {
-    private File fileCSV;
+    private File fileCSV = new File("");
 
     public void setFileCSV(File fileCSV) {
         this.fileCSV = fileCSV;
